ad61f6bd318594b1c32e4fba5803a24e76cd3b24
Luis <lluque@c2r6s3.42malaga.com>
2024-11-26 20:42:12 +0100

    IMPROVED cleaner UI, base listener inits function
    A function was added to group every listener for the base components
    of the UI. A new .js file was added to group every future init
    functions.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('login-button').addEventListener('click', function() {
-        addResult("https://substackcdn.com/image/fetch/f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F476763b3-cd81-41db-9905-f12bf3485b58_933x339.png");
-        //addResult("https://www.nobelprize.org/images/hayek-13256-portrait-medium.jpg")
-    });
-
-});
+init_base_listeners();
