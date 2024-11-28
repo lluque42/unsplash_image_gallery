7e66067ecee2f4be15eb514312ccf83b967d7b3b
Luis Luque <lluque@student.42malaga.com>
2024-11-28 22:37:07 +0100

    ADD Images from an API request showing
    At load, the page now shows the 10 images referenced by the response
    to the basic request made to the Unsplash API. In the results
    grid the images were added to those hardcoded for testing. The images
    from Unsplash also show a rudimentary tooltip with image metadata.
    
    The ID of each picture is now included in the corresponding html
    element for further manipulation. Also the event listeners for the
    image favorite button and for detecting a click on the image to open
    the full size versions were added.

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,24 @@ async function main() {
     init_base_listeners();
     let endpoint = 'https://api.unsplash.com/photos/?client_id=dccWT6c-O9-cyli0Tcr-aBF7OWqeI2UzE-5RlsgVkRA';
     let apiResponseJson;
+    // This await is necessary to wait for the response from the function
+    // This function returns a promise (or something that depends on a promise)
     apiResponseJson = await requestFirstImagesStyle1(endpoint);
     console.log(`The style 1 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
+    // This await is necessary to wait for the response from the function
+    // This function returns a promise (or something that depends on a promise)
     apiResponseJson = await requestFirstImagesStyle2(endpoint);
     console.log(`The style 2 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
+    // This await is necessary to wait for the response from the function
+    // This function returns a promise (or something that depends on a promise)
     apiResponseJson = await requestFirstImagesStyle3(endpoint);
     console.log(`The style 3 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
+
+    apiResponseJson.forEach(element => {
+        console.log(element.id);
+        let imgResult = parseApiResult(element);
+        addResult(imgResult);
+    });
+    
 }
+
--- scripts/result-composing.js
+++ scripts/result-composing.js
@@ -1,20 +1,80 @@
-function addResult(url) {
+/*
+function createImgResult() {
+    return {
+        "id": "123",
+        "alt": "A cute cat",
+        "height": 200,
+        "width": 300,
+        "url_thumbnail": "https://example.com/cat.jpg",
+        "url_full": "https://example.com/cat.jpg",
+        "url_small": "https://example.com/cat.jpg",
+    };
+}
+*/
+
+function parseApiResult(apiResult) {
+    return {
+        id: apiResult.id,
+        alt: apiResult.alt_description,
+        height: apiResult.height,
+        width: apiResult.width,
+        url_thumbnail: apiResult.urls.thumb,
+        url_full: apiResult.urls.full,
+        url_small: apiResult.urls.small,
+    };
+}
+
+function addResult(imgResult) {
     const resultsContainer = document.getElementById('results-container')
     const imageContainer = document.createElement('div');
     const imageThumbnail = document.createElement('img');
     const favButton = document.createElement('button');
+    const tooltipText = document.createElement('span');
 
     imageContainer.classList.add('image-container');
+    imageContainer.classList.add('tooltip');    // To be toopltipeable
     imageThumbnail.classList.add('image-thumbnail');
     favButton.classList.add('fav-button-add');
+    tooltipText.classList.add('tooltip-text');  // The hidden tooltip text that will be displayed on hover
 
+    imageContainer.appendChild(tooltipText);
     imageContainer.appendChild(imageThumbnail);
     imageContainer.appendChild(document.createElement('br'));
     imageContainer.appendChild(favButton);
     resultsContainer.appendChild(imageContainer);
+    
 
     favButton.innerHTML = 'Add to favorites';
-    
+    tooltipText.innerHTML = `Size (hxw): ${imgResult.height} x ${imgResult.width}\n Description: ${imgResult.alt}`;  //
  
-    imageThumbnail.src = url;
+    imageThumbnail.src = imgResult.url_thumbnail;
+    imageContainer.id = imgResult.id;
+    imageThumbnail.alt = imgResult.alt;
+
+
+    // NOTICE THAT IN THIS CASE WE DONT USE ANONYMOUS FUNCTIONS
+    favButton.addEventListener('click', function(event) {
+        toggleFav(event);
+    });
+
+    imageThumbnail.addEventListener('click', function(event) {
+        openImg(event);
+    });
+/*
+    imageContainer.addEventListener('hover', function() {
+        console.log('Hovered over image');
+    })
+*/
+}
+
+function toggleFav(event) {
+    console.log('Added to favorites', event.target.parentElement.id);
+}
+
+function openImg(event) {
+    console.log('Clicked on image', event.target.parentElement.id);
+}
+
+function hintOnImg(event) {
+    console.log('Hovered over image',event.target.parentElement.id);
 }
