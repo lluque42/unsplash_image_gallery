1f0052ad0f742a56a4b8ebbcc27bd40c6f1f3ba2
Luis Luque <lluque@student.42malaga.com>
2024-11-29 16:03:51 +0100

    ADD separate file for API credentials
    A new env.json file was added to store the private keys.
    
    Not the best approach because:
        * It should not be in json format, but as key value pairs.
            * I didn't want to waste time yet parsing that.
        * It should be a hidden file named .env.
            * I had problem fetching() hidden files due to security
            reasons.
        * It should be accessible only to the backend and provided
        by it to the frontend (or consume the apis in the backend).
            * No separate backend is planned for this project yet.
        * This file was added to the .gitignore not to be publicly
        available in the repository. Instead a template was pushed.
    But:
        * At least the to the keys are already centralized, encapsulated
        and accessed in the same way and through a single contact point.

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,40 @@
 main();
 
+let credentials;
 // This function is async in order to use the await keyword
 // when calling other async functions
 async function main() {
     init_base_listeners();
-    let endpoint = 'https://api.unsplash.com/photos/?client_id=dccWT6c-O9-cyli0Tcr-aBF7OWqeI2UzE-5RlsgVkRA';
+    const unsplashApiBaseUrl = 'https://api.unsplash.com';
+    let endpoint = `/photos/`;
+    credentials = await loadCredentials();
+    //console.log(credentials);
+    //console.log(await credentials.ACCESS_KEY);
+    let query = `?client_id=${await credentials.ACCESS_KEY}`;
+    let url = `${unsplashApiBaseUrl}${endpoint}${query}`;
+    console.log(`The URL is: ${url}`);
     let apiResponseJson;
     // This await is necessary to wait for the response from the function
     // This function returns a promise (or something that depends on a promise)
-    apiResponseJson = await requestFirstImagesStyle1(endpoint);
+    apiResponseJson = await requestFirstImagesStyle1(url);
     console.log(`The style 1 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
     // This await is necessary to wait for the response from the function
     // This function returns a promise (or something that depends on a promise)
-    apiResponseJson = await requestFirstImagesStyle2(endpoint);
-    console.log(`The style 2 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
+    //apiResponseJson = await requestFirstImagesStyle2(endpoint);
+    //console.log(`The style 2 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
     // This await is necessary to wait for the response from the function
     // This function returns a promise (or something that depends on a promise)
-    apiResponseJson = await requestFirstImagesStyle3(endpoint);
-    console.log(`The style 3 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
+    //apiResponseJson = await requestFirstImagesStyle3(endpoint);
+    //console.log(`The style 3 output:`, JSON.parse(JSON.stringify(apiResponseJson)));
 
+    
     apiResponseJson.forEach(element => {
         console.log(element.id);
         let imgResult = parseApiResult(element);
         addResult(imgResult);
     });
+    
+
     
 }
 
