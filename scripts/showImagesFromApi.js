9b5f56b47016aa56dd802ea5a6dcff9c9e278594
Luis Luque <luque.luis@gmail.com>
2024-12-01 11:31:52 +0100

    FIX New search deletes old results

--- scripts/showImagesFromApi.js
+++ scripts/showImagesFromApi.js
@@ -1,4 +1,9 @@
 function showImagesFromApi(arrayApiResponseForImages) {
+	const resultsContainer = document.getElementById('results-container');
+	// This also removes every event listener, freeing resources
+	while (resultsContainer.firstChild) {
+		resultsContainer.removeChild(resultsContainer.firstChild);
+	}
 	arrayApiResponseForImages.forEach(imageApiData => {
 		console.log(imageApiData.id);
 		let imgResult = parseApiResult(imageApiData);
